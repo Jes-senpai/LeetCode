c79e469882953ae97b99ad10803f53b0f0fad1dd
Jes-senpai <70440162+Jes-senpai@users.noreply.github.com>
2021-07-21 13:50:32 +0530

    Time: 0 ms (100.00%), Space: 39 MB (72.06%) - LeetHub

---
+++ next-permutation/next-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+       int i = nums.length - 2;   
+        while (i >= 0 && nums[i + 1] <= nums[i]) 
+        {
+		   i--;
+        }
+        if (i >= 0) 
+        {
+            int j = nums.length - 1; 
+			while (nums[j] <= nums[i]) 
+            {
+                j--;
+            }
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        int start = i+1,end = nums.length-1;
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    
+}
