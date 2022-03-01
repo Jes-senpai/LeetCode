42c633699888afc51cb87835e36baf01071be65b
Jes-senpai <70440162+Jes-senpai@users.noreply.github.com>
2022-03-01 10:59:39 +0530

    Time: 2 ms (48.04%), Space: 50 MB (17.77%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.java
@@ -1,27 +1,19 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
+        int l = 0,r = numbers.length-1;
         int[] res = new int[2];
-        for(int i=0;i<numbers.length-1;i++){
-            int x = find(numbers,i+1,target-numbers[i]);
-            if(x!=-1){
-                res[0] = i+1;
-                res[1] = x+1;
+        while(l<r){
+            int sum = numbers[l]+numbers[r];
+            if(sum==target){
+                res[0] = l+1;
+                res[1] = r+1;
                 return res;
             }
+            if(sum>target)
+                r--;
+            if(sum<target)
+                l++;
         }
         return res;
-    }
-    public int find(int[] nums,int l,int t){
-        int r = nums.length-1;
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid]==t)
-                return mid;
-            if(nums[mid]<t)
-                l = mid+1;
-            if(nums[mid]>t)
-                r = mid-1;
-        }
-        return -1;
     }
 }
